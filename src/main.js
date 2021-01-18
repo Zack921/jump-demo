73a4bfa5ca79af1c27b5ccaf983c380f8428d448
zackguo <zackguo@tencent.com>
2021-01-18 15:19:52 +0800

    feat: 旋转三角形终版

--- src/main.js
+++ src/main.js
@@ -4,31 +4,31 @@ window.THREE = THREE;
 class Main {
   constructor() {};
   init() {
-    var width = document.body.style.width.replace('px', '');
-    var height = document.body.style.height.replace('px', '');
+    const width = document.body.style.width.replace('px', '');
+    const height = document.body.style.height.replace('px', '');
 
-    var renderer = new THREE.WebGLRenderer({
+    const renderer = new THREE.WebGLRenderer({
       canvas: canvas
     });
-    var scene = new THREE.Scene();
-    var camera = new THREE.OrthographicCamera(-width / 2, width / 2,
+    const scene = new THREE.Scene();
+    const camera = new THREE.OrthographicCamera(-width / 2, width / 2,
     height / 2, -height / 2, -1000, 1000);
 
     renderer.setClearColor(new THREE.Color(0x000000));
     renderer.setSize(400, 400);
 
-    var triangleShape = new THREE.Shape();
+    const triangleShape = new THREE.Shape();
     triangleShape.moveTo(0, 100);
     triangleShape.lineTo(-100, -100);
     triangleShape.lineTo(100, -100);
     triangleShape.lineTo(0, 100);
 
-    var geometry = new THREE.ShapeGeometry(triangleShape);
-    var material = new THREE.MeshBasicMaterial({
+    const geometry = new THREE.ShapeGeometry(triangleShape);
+    const material = new THREE.MeshBasicMaterial({
       color: 0xff0000,
       side: THREE.DoubleSide
     });
-    var mesh = new THREE.Mesh(geometry, material);
+    const mesh = new THREE.Mesh(geometry, material);
     mesh.position.x = 0;
     mesh.position.y = 0;
     mesh.position.z = 0;
@@ -39,10 +39,10 @@ class Main {
     camera.position.z = 1;
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    var currentAngle = 0;
-    var lastTimestamp = Date.now();
+    let currentAngle = 0;
+    let lastTimestamp = Date.now();
 
-    var animate = function () {
+    function animate () {
       var now = Date.now();
       var duration = now - lastTimestamp;
       lastTimestamp = now;
@@ -50,7 +50,7 @@ class Main {
       if(currentAngle >= 2 * Math.PI) currentAngle = 0;
     }
 
-    var render = function () {
+   function render () {
       animate();
       mesh.rotation.set(0, 0, currentAngle);
       renderer.render(scene, camera);
